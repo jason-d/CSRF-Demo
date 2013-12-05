5097ccb7b0a7f60c968a253a0778dd9e0296d1e8
Jason <jason.d.ody@gmail.com>
2013-12-05 14:33:52 -0500

    Initial setup of app

---
+++ app/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var controller = require('./controller.js');
+
+var app = express();
+
+var notFoundHandler = function(req, res) {
+    res.statusCode = 404;
+    res.description = 'Page not found.';
+    res.render('notFound');
+};
+
+var errorHandler = function(err, req, res, next) {
+    console.log("****ERROR****");
+    console.log(err);
+    
+    res.statusCode = 500;
+    res.description = 'Something went wrong';
+    res.render('error');
+};
+
+app.configure(function() {
+    app.set('view engine', 'jade');
+    app.set('views', __dirname + '/views');
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.bodyParser());
+    app.use(app.router); //Enable error handling
+    app.use(notFoundHandler); //If no routes match, this will be called
+    app.use(errorHandler); //Express knows a method with 4 params, is for handling errors
+});
+
+app.listen(process.env.PORT, process.env.IP);
+console.log('csrf-demo running...');
